b4073a276584953aa1f6016212a327595eb70337
劉軒宏 <3b032110@gm.student.ncut.edu.tw>
2023-10-17 19:48:52 +0800

    - Change Route GET hello to  hello.index - Add src/views/welcome.blade some route link

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,13 @@ Route::get('r2', function () {
 // });
 
 
+
+// Route::get('hello/{name?}', function ($name = "Everybody") {
+//     return 'Hello, '.$name;
+// });
+
+
+
 Route::get('hello/{name?}', function ($name = "Everybody") {
     return 'Hello, '.$name;
-});
+})->name('hello.index');
